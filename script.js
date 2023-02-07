d7b2e664f4358110f320badc3e19bad609f0a914
Butch Bet <butchbet01@gmail.com>
2023-02-07 08:31:23 -0500

    finished the project.

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
+const resultList = document.getElementById("result");
 const resultItems = document.getElementsByClassName("result__item");
 const button = document.getElementById("button");
 
 let resultStorage = [];
 const result = "wesbos";
+let valiationInterval = null;
 
-button.addEventListener("click", checkResult);
-window.addEventListener("keyup", changeResult);
+button.addEventListener("click", checkResultStorage);
+window.addEventListener("keyup", changeResultStorage);
 
-function checkResult() {
+function checkResultStorage() {
+    clearInterval(valiationInterval);
+
     if(resultStorage.join("").includes(result)) {
-        console.log("Yes!");
-        cornify_add();
+
+        resultList.classList.add("correct");
+        
+        
+    } else {
+        resultList.classList.add("wrong");
+    }
+    
+    valiationInterval = setInterval(() => {
+        resultList.classList.remove("correct");
+        resultList.classList.remove("wrong");
+    }, 1000);
+}
+
+function changeResultStorage(e) {
+    if(e.key == "Enter" || e.key == " ") {
+        checkResultStorage();
+    } else {
+        resultStorage.push(e.key);   
+        
+        resultStorage.splice(-result.length - 1, resultStorage.length - result.length);
+    
+        fillResultList();
     }
 }
 
-function changeResult(e) {
-    resultStorage.push(e.key);   
-    
-    resultStorage.splice(-result.length - 1, resultStorage.length - result.length);
-
-    checkResult();
+function fillResultList() {
+    for(let i = resultItems.length - 1, k = resultStorage.length - 1;  i > resultItems.length - resultStorage.length - 1; i--, k--) {
+        resultItems[i].textContent = resultStorage[k];
+    }
 }
